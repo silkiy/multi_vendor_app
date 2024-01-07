29cecc62941a54a2e5c0c5c250c0b98838fa481b
Silki <wildanmilk@gmail.com>
2024-01-07 17:09:35 +0700

    Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:multi_vendor_app/firebase_options.dart';
 import 'package:multi_vendor_app/views/buyers/main_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
